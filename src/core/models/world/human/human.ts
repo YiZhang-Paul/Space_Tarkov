dab7085d2dd8c6733c46ab640fd15502ec37152a
Yi Zhang <yi.zhang9206@gmail.com>
2023-05-21 22:04:42 -0400

    update user state

--- src/core/models/world/human/human.ts
+++ src/core/models/world/human/human.ts
@@ -1,5 +1,6 @@
 import { useControlStore } from '../../../../stores/control.store';
 import { Stack } from '../../generic/stack';
+import { KeyValuePair } from '../../generic/key-value-pair';
 import { MoveOption } from '../move-option';
 import { SolidObject } from '../solid-object';
 import { HumanState } from '../../../enums/human-state.enum';
@@ -14,7 +15,16 @@ export class Human extends SolidObject {
 
     public initialize(): this {
         this._motorSystem.initialize(this._graphics, this._sceneStore.cameraHeight * 0.02);
-        this._motorSystem.onBoostingCompleted = () => this._states.swap(HumanState.Boosted);
+
+        this._motorSystem.onBoostingCompleted = () => {
+            if (this._controlStore.isBoostKeyPressed) {
+                this._states.swap(HumanState.Boosted);
+            }
+            else {
+                this._states.pop();
+            }
+        };
+
         this._motorSystem.onFreeFallCompleted = () => this._states.pop();
         this._graphics.pivot.set(this._graphics.width / 2, this._graphics.height / 2);
         this._sceneStore.addToStage(this._graphics);
@@ -24,6 +34,7 @@ export class Human extends SolidObject {
 
     public update(): void {
         this.updateOrientation();
+        this.updateState();
         this.updateMovement();
     }
 
@@ -40,9 +51,94 @@ export class Human extends SolidObject {
         this.orientation = isRight ? Orientation.Right : Orientation.Left;
     }
 
+    private updateState(): void {
+        const state = this._states.peek();
+
+        if (state === HumanState.Boosting || state === HumanState.FreeFall) {
+            return;
+        }
+
+        if (this._controlStore.isBoostKeyPressed) {
+
+            if (state === HumanState.Boosted) {
+                return;
+            }
+
+            this._states.push(HumanState.FreeFall);
+            this._states.push(HumanState.Boosting);
+
+            return;
+        }
+
+        if (state === HumanState.Boosted) {
+            this._states.pop();
+
+            return;
+        }
+
+        const direction = this.getMovingDirection();
+
+        if (!direction) {
+
+            if (state === HumanState.Idle) {
+                return;
+            }
+
+            this._states.pop();
+
+            return;
+        }
+
+        if (this._controlStore.isSprintKeyPressed) {
+
+            if (state === HumanState.Sprint) {
+                return;
+            }
+
+            if (state === HumanState.Idle) {
+                this._states.push(HumanState.Sprint);
+            }
+            else {
+                this._states.swap(HumanState.Sprint);
+            }
+
+            return;
+        }
+
+        if (state === HumanState.Idle) {
+            this._states.push(HumanState.Walk);
+        }
+    }
+
     private updateMovement(): void {
         const boostHeight = this._sceneStore.cameraHeight / 30;
-        const option = new MoveOption(this.orientation, this.orientation, boostHeight);
+        const option = new MoveOption(this.orientation, this.getMovingDirection(), boostHeight);
         this._motorSystem.update(this._states.peek(), option);
+    }
+
+    private getMovingDirection(): Orientation | null {
+        const { moveUp, moveDown, moveLeft, moveRight } = this._controlStore.controlKeys;
+        const verticalKeys = [new KeyValuePair(moveUp, Orientation.Up), new KeyValuePair(moveDown, Orientation.Down)];
+        const horizontalKeys = [new KeyValuePair(moveLeft, Orientation.Left), new KeyValuePair(moveRight, Orientation.Right)];
+        const vertical = this._controlStore.lastKeyPressed(verticalKeys)?.value ?? null;
+        const horizontal = this._controlStore.lastKeyPressed(horizontalKeys)?.value ?? null;
+
+        if (vertical === Orientation.Up && horizontal === Orientation.Left) {
+            return Orientation.UpLeft;
+        }
+
+        if (vertical === Orientation.Up && horizontal === Orientation.Right) {
+            return Orientation.UpRight;
+        }
+
+        if (vertical === Orientation.Down && horizontal === Orientation.Left) {
+            return Orientation.DownLeft;
+        }
+
+        if (vertical === Orientation.Down && horizontal === Orientation.Right) {
+            return Orientation.DownRight;
+        }
+
+        return vertical ? vertical : horizontal;
     }
 }
