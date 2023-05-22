7fea5dd0c05fade1bde162d4856222b2bb657e9e
Yi Zhang <yi.zhang9206@gmail.com>
2023-05-22 00:01:30 -0400

    fixed bug when trying to evaulate truthy/falsy for orientations by avoiding default 0

--- src/core/enums/orientation.enum.ts
+++ src/core/enums/orientation.enum.ts
@@ -1,10 +1,10 @@
 export enum Orientation {
-    Up,
-    Down,
-    Left,
-    Right,
-    UpLeft,
-    UpRight,
-    DownLeft,
-    DownRight
+    Up = 1,
+    Down = 2,
+    Left = 3,
+    Right = 4,
+    UpLeft = 5,
+    UpRight = 6,
+    DownLeft = 7,
+    DownRight = 8
 }
