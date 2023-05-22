4ae3f06e7a21c03349cba800a188cabb9aaa4612
Yi Zhang <yi.zhang9206@gmail.com>
2023-05-21 20:50:58 -0400

    added boosting movement

---
+++ src/core/models/world/human/movements/boosting-movement.ts
@@ -0,0 +1,21 @@
+import type { SimpleCounter } from '../../../generic/simple-counter';
+import type { MoveOption } from '../../move-option';
+import type { HumanBodyParts } from '../human-body-parts';
+import type { HumanState } from '../../../../enums/human-state.enum';
+
+import { HumanMovement } from './human-movement';
+
+export class BoostingMovement extends HumanMovement<SimpleCounter<HumanState>> {
+
+    public update(bodyParts: HumanBodyParts, counter: SimpleCounter<HumanState>, option: MoveOption): void {
+        this.lean(bodyParts, option);
+        this.breath(bodyParts, bodyParts.trunk.height / 20 * counter.progress);
+        this.flyUp(bodyParts, counter, option);
+    }
+
+    private flyUp(bodyParts: HumanBodyParts, counter: SimpleCounter<HumanState>, option: MoveOption): void {
+        if (bodyParts.graphics.y < option.boostHeight) {
+            bodyParts.graphics.y -= option.boostHeight * counter.progress;
+        }
+    }
+}
